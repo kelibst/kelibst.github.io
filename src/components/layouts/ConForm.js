135385ddf22e786672b8f29b4ad0346bc9fe1c1a
Keli Booster <kbooster17@gmail.com>
2021-01-16 01:57:00 +0000

    handle form sent

--- src/components/layouts/ConForm.js
+++ src/components/layouts/ConForm.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import * as emailjs from "emailjs-com";
+
 
 const ConForm = () => {
   const initialState = {
@@ -8,9 +10,79 @@ const ConForm = () => {
     message: "",
   };
   const [userData, setUserData] = useState(initialState);
-  console.log(userData);
+  const [isSubmit, setisSubmit] = useState(false) 
+  const [sent, setSend] = useState(
+    {
+      status: false,
+      errors: false,
+    }
+  )
+const { status , errors } = sent
+  const { fullname, email, phone, message } = userData;
   const handleSubmit = (e) => {
     e.preventDefault();
+    setisSubmit(true)
+
+    const templateParams = {
+      from_name: fullname.trim(),
+      from_email: email.trim(),
+      subject: "",
+      phone: phone.trim(),
+      to_name: "Kekeli Dogbevi Jiresse",
+      message: message.trim(),
+    };
+    emailjs
+      .send(
+        "service_ao4tyqi",
+        "template_a1tgwwi",
+        templateParams,
+        "user_t3BxG9skb7zXIqBIK1Cdu"
+      )
+      .then(() => {
+        setisSubmit(false)
+        resetForm();
+        setSend({
+          status: true
+        })
+        
+      }).catch(err => {
+        setSend({
+          errors: true
+        })
+       
+        setisSubmit(false)
+      });
+  };
+
+    useEffect(() => {
+     status && setTimeout(() => {
+        setSend({
+          status: false
+        })
+      }, 5000)
+
+      errors &&  setTimeout(() => {
+        setSend({
+          errors: false
+        })
+      }, 5000)
+    },[status, errors])
+  const handleChange = (e) => {
+    const { target } = e;
+    const { name, value } = target;
+    setUserData({
+      ...userData,
+      [name]: value,
+    });
+  };
+
+  const resetForm = () => {
+    setUserData({
+      fullname: "",
+      email: "",
+      phone: "",
+      message: "",
+    });
   };
   return (
     <div className="con-form">
@@ -21,15 +93,27 @@ const ConForm = () => {
             required
             type="text"
             name="fullname"
+            onChange={handleChange}
             placeholder="Full name"
+            value={fullname}
           />
-          <input type="email" required name="email" placeholder="Email" />
           <input
-            type="phone"
+            type="email"
+            onChange={handleChange}
+            value={email}
+            required
+            name="email"
+            placeholder="Email"
+          />
+          <input
+            type="tel"
+            pattern="^(\+)?([ 0-9]){9,16}$"
             required
             className="phone-num"
             name="phone"
+            onChange={handleChange}
             placeholder="Phone number"
+            value={phone}
           />
         </div>
         <p>Message</p>
@@ -40,10 +124,18 @@ const ConForm = () => {
             name="message"
             rows="10"
             className="con-area"
+            onChange={handleChange}
+            value={message}
           ></textarea>
-          <button type="submit" className="con-sub btn">
+          {isSubmit ? <button type="submit" disabled className="con-sub btn">
+          Send Message
+        </button> : 
+       <button type="submit" className="con-sub btn">
             Send Message
           </button>
+      }
+      { status &&  <h6 className={`status d-block text-succ text-center`}>Sent</h6>}
+       { errors &&   <h6 className={`status text-danger text-center`}>There was an Error in Sending your message. Try again later.</h6>}
         </div>
       </form>
     </div>
